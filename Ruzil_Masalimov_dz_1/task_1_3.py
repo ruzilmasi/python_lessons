d8c0289c8b2bbe6f24c5838d0ab6f4dc1eb78f2d
ruzilmasi <ruzilmasic@gmail.com>
2022-03-04 16:49:33 +0300

    правка задания 1_3 добавление условия
        if 15 > el > 10:
            print(f'{el} процентов')

--- Ruzil_Masalimov_dz_1/task_1_3.py
+++ Ruzil_Masalimov_dz_1/task_1_3.py
@@ -1,5 +1,7 @@
 for el in range(1, 101):
-    if el % 10 == 1:
+    if 15 > el > 10:
+        print(f'{el} процентов')
+    elif el % 10 == 1:
         print(f'{el} процент')
     elif 5> el % 10 > 1:
         print(f'{el} процента')
